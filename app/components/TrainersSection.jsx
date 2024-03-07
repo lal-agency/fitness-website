f21db31400043724796603554a1ec1a3f95dc450
Griffin Annshual <griffintbr@gmail.com>
2024-03-07 14:34:16 +0530

    Fix bugs in trainer container ui

--- app/components/TrainersSection.jsx
+++ app/components/TrainersSection.jsx
@@ -28,143 +28,146 @@ const TrainersSection = () => {
 								</p>
 							</div>
 						</div>
-						<div className="row">
-							{/* Trainer 1 */}
-							<div className="col-lg-4 hover:scale-105 transition-all ease-in-out">
-								<div className="trainer-item">
-									<div className="image-thumb">
-										<Image
-											src="/assets/images/first-trainer.jpg"
-											alt=""
-											width={250} // Set appropriate width for the image
-											height={200} // Set appropriate height for the image
-										/>
-									</div>
-									<div className="down-content hover:scale-105 transition-all ease-in-out">
-										<span>Strength Trainer</span>
-										<h4>Bret D. Bowers</h4>
-										<p>
-											With over a decade of experience in strength training,
-											Bret D. Bowers is dedicated to helping individuals
-											maximize their strength potential and achieve their
-											fitness goals.
-										</p>
+						{/* Trainer 1 */}
+						<div className=" md:px-0 px-10 hover:scale-105 transition-all ease-in-out">
+							<div className="trainer-item">
+								<div className="image-thumb">
+									<Image
+										src="/assets/images/first-trainer.jpg"
+										alt=""
+										width={250} // Set appropriate width for the image
+										height={200} // Set appropriate height for the image
+									/>
+								</div>
+								<div className="down-content hover:scale-105 transition-all ease-in-out">
+									<span>Strength Trainer</span>
+									<h4>Bret D. Bowers</h4>
+									<p>
+										With over a decade of experience in strength training, Bret
+										D. Bowers is dedicated to helping individuals maximize their
+										strength potential and achieve their fitness goals.
+									</p>
 
-										<ul className="social-icons">
-											<li>
-												<a href="#">
-													<i className="fa fa-facebook"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-twitter"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-linkedin"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-behance"></i>
-												</a>
-											</li>
-										</ul>
-									</div>
+									<ul
+										className="social-icons"
+										id="trainer-icons">
+										<li>
+											<a href="#">
+												<i className="fa fa-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-linkedin"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-behance"></i>
+											</a>
+										</li>
+									</ul>
 								</div>
 							</div>
-							{/* Trainer 2 */}
-							<div className="col-lg-4 hover:scale-105 transition-all ease-in-out">
-								<div className="trainer-item">
-									<div className="image-thumb">
-										<Image
-											src="/assets/images/second-trainer.jpg"
-											alt=""
-											width={250} // Set appropriate width for the image
-											height={200} // Set appropriate height for the image
-										/>
-									</div>
-									<div className="down-content hover:scale-105 transition-all ease-in-out">
-										<span>Muscle Trainer</span>
-										<h4>Hector T. Daigl</h4>
-										<p>
-											Hector T. Daigl is a seasoned fitness instructor with a
-											passion for helping others achieve their health and
-											wellness goals through personalized training programs and
-											motivational support.
-										</p>
+						</div>
+						{/* Trainer 2 */}
+						<div className=" md:px-0 px-10 hover:scale-105 transition-all ease-in-out">
+							<div className="trainer-item">
+								<div className="image-thumb">
+									<Image
+										src="/assets/images/second-trainer.jpg"
+										alt=""
+										width={250} // Set appropriate width for the image
+										height={200} // Set appropriate height for the image
+									/>
+								</div>
+								<div className="down-content hover:scale-105 transition-all ease-in-out">
+									<span>Muscle Trainer</span>
+									<h4>Hector T. Daigl</h4>
+									<p>
+										Hector T. Daigl is a seasoned fitness instructor with a
+										passion for helping others achieve their health and wellness
+										goals through personalized training programs and
+										motivational support.
+									</p>
 
-										<ul className="social-icons">
-											<li>
-												<a href="#">
-													<i className="fa fa-facebook"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-twitter"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-linkedin"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-behance"></i>
-												</a>
-											</li>
-										</ul>
-									</div>
+									<ul
+										className="social-icons"
+										id="trainer-icons">
+										<li>
+											<a href="#">
+												<i className="fa fa-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-linkedin"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-behance"></i>
+											</a>
+										</li>
+									</ul>
 								</div>
 							</div>
-							{/* Trainer 3 */}
-							<div className="col-lg-4 hover:scale-105 transition-all ease-in-out">
-								<div className="trainer-item">
-									<div className="image-thumb">
-										<Image
-											src="/assets/images/third-trainer.jpg"
-											alt=""
-											width={250} // Set appropriate width for the image
-											height={200} // Set appropriate height for the image
-										/>
-									</div>
-									<div className="down-content hover:scale-105 transition-all ease-in-out">
-										<span>Power Trainer</span>
-										<h4>Paul D. Newman</h4>
-										<p>
-											Paul D. Newman is a certified personal trainer with a
-											focus on holistic wellness and functional fitness. With
-											his expertise and guidance, you can embark on a journey
-											towards a healthier and happier lifestyle.
-										</p>
+						</div>
+						{/* Trainer 3 */}
+						<div className=" md:px-0 px-10 hover:scale-105 transition-all ease-in-out">
+							<div className="trainer-item">
+								<div className="image-thumb">
+									<Image
+										src="/assets/images/third-trainer.jpg"
+										alt=""
+										width={250} // Set appropriate width for the image
+										height={200} // Set appropriate height for the image
+									/>
+								</div>
+								<div className="down-content hover:scale-105 transition-all ease-in-out">
+									<span>Power Trainer</span>
+									<h4>Paul D. Newman</h4>
+									<p>
+										Paul D. Newman is a certified personal trainer with a focus
+										on holistic wellness and functional fitness. With his
+										expertise and guidance, you can embark on a journey towards
+										a healthier and happier lifestyle.
+									</p>
 
-										<ul className="social-icons">
-											<li>
-												<a href="#">
-													<i className="fa fa-facebook"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-twitter"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-linkedin"></i>
-												</a>
-											</li>
-											<li>
-												<a href="#">
-													<i className="fa fa-behance"></i>
-												</a>
-											</li>
-										</ul>
-									</div>
+									<ul
+										className="social-icons"
+										id="trainer-icons">
+										<li>
+											<a href="#">
+												<i className="fa fa-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-linkedin"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i className="fa fa-behance"></i>
+											</a>
+										</li>
+									</ul>
 								</div>
 							</div>
 						</div>
